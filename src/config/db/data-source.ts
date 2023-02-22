4a26fbfcea416850aca49ce4394ebb777b21a391
Aiandra  Meira <33138081+aiandralves@users.noreply.github.com>
2023-02-22 12:49:16 -0300

    Adicionando a migration de followers

--- src/config/db/data-source.ts
+++ src/config/db/data-source.ts
@@ -7,6 +7,7 @@ import { Users1676908790120 } from "src/infra/db/migrations/1676908790120-Users"
 import { Profile1676910803076 } from "src/infra/db/migrations/1676910803076-Profile";
 import { Posts1676915839776 } from "src/infra/db/migrations/1676915839776-Posts";
 import { Photos1677019758604 } from "src/infra/db/migrations/1677019758604-Photos";
+import { Followers1677074973317 } from "src/infra/db/migrations/1677074973317-Followers";
 import { DataSource, DataSourceOptions } from "typeorm";
 
 ConfigModule.forRoot();
@@ -18,7 +19,13 @@ export const dataSourceOptions: DataSourceOptions = {
     password: process.env.TYPEORM_PASSWORD,
     database: process.env.TYPEORM_DATABASE,
     entities: [User, Profile, Post, Photo],
-    migrations: [Users1676908790120, Profile1676910803076, Posts1676915839776, Photos1677019758604],
+    migrations: [
+        Users1676908790120,
+        Profile1676910803076,
+        Posts1676915839776,
+        Photos1677019758604,
+        Followers1677074973317,
+    ],
     synchronize: false,
 } as DataSourceOptions;
 
---
+++ src/infra/db/migrations/1677074973317-Followers.ts
@@ -0,0 +1,68 @@
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from "typeorm";
+
+export class Followers1677074973317 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "followers",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        length: "11",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "userId",
+                        type: "int",
+                        length: "11",
+                    },
+                    {
+                        name: "followerId",
+                        type: "int",
+                        length: "11",
+                    },
+                    {
+                        name: "createdAt",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                    {
+                        name: "updatedAt",
+                        type: "timestamp",
+                        isNullable: true,
+                        default: "now()",
+                    },
+                ],
+            }),
+        );
+
+        await queryRunner.createForeignKey(
+            "followers",
+            new TableForeignKey({
+                name: "fk_followers_userId",
+                columnNames: ["userId"],
+                referencedTableName: "users",
+                referencedColumnNames: ["id"],
+            }),
+        );
+
+        await queryRunner.createForeignKey(
+            "followers",
+            new TableForeignKey({
+                name: "fk_followers_followerId",
+                columnNames: ["followerId"],
+                referencedTableName: "users",
+                referencedColumnNames: ["id"],
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropForeignKey("followers", "fk_followers_userId");
+        await queryRunner.dropForeignKey("followers", "fk_followers_followerId");
+        await queryRunner.dropTable("followers");
+    }
+}
