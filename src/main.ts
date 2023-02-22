5566b31e83bb24053863de535478bd1bcf7d67f1
Aiandra  Meira <33138081+aiandralves@users.noreply.github.com>
2023-02-22 13:57:52 -0300

    Instalando o swagger no projeto

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { NestExpressApplication } from "@nestjs/platform-express";
+import { DocumentBuilder } from "@nestjs/swagger";
+import { SwaggerModule } from "@nestjs/swagger/dist";
 import { join } from "path";
 import { AppModule } from "./app.module";
 
@@ -8,6 +10,15 @@ async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
     app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
     app.useStaticAssets(join(__dirname, "..", "client"), { prefix: "/public" });
+
+    const config = new DocumentBuilder()
+        .setTitle("Social Media API")
+        .setDescription("Desenvolvimento de uma API para social media")
+        .setVersion("0.0.1")
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("swagger", app, document);
+
     await app.listen(3333);
 }
 bootstrap();
